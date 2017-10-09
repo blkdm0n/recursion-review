c8b6b4b80cd6c511b35db8e9fcbbda0bd6265e64
blkdm0n <bernard.kitchens@gmail.com>
2017-10-09 11:44:47 -0700

    1st commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+//inputs: classname
+//output = an array of all elements matchign that classanme
+//constraints = no native methods, use recusion
+//edge: n/a
+
+
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var results = [];
+  var element = element || document.body;
+
+  //if there are no more children nodes, return results
+
+  if (!element.childNodes.length) {
+    return results;
+  }
+
+  //else recursive call (getElementsByClassName) on current element
+
+  //look at an element
+  if (element.className === className) {
+    results.push(element);
+    return getElementsByClassName();
+    
+     //if it matches class name
+       //push the element to the array  
+  }  
 };
