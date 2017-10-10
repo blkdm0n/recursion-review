122916f7d566abbca428bbad5a8375ae75284395
blkdm0n <bernard.kitchens@gmail.com>
2017-10-09 21:01:30 -0700

    getElementsByClassName test passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,10 +11,40 @@
 //edge: n/a
 
 
+// var getElementsByClassName = function(className, element) {
+//   var results = [];
+//   var element = element || document.body;
+//   var child = element.children;
+//   console.log('children method', child[1].className);
+//   var child2 = element.childNodes;
+//   console.log('childNodes method', child2[3].className);
+
+//   //if there are no more children nodes, return results
+
+//   if (!child.length) {
+//     return results;
+//   } else {
+//     console.log(element);
+//   //else recursive call (getElementsByClassName) on current element
+//     for (var i = 0; i < child.length; i++) {
+//       if (child[i].className === className) {
+//         results.push(child[i]);
+//         results = results.concat(getElementsByClassName(className, child[i]));
+//         console.log(results);
+    
+//      //if it matches class name
+//        //push the element to the array  
+//       } 
+//     }
+
+//     return results;
+//   }
+// };
+
 var getElementsByClassName = function(className, element) {
   var results = [];
   var element = element || document.body;
-  var child = element.children;
+  var child = element.childNodes;
   
   console.log("Element", element);
   console.log("Child node here", element.childNodes);
@@ -28,10 +58,12 @@ var getElementsByClassName = function(className, element) {
   if (child.length) {
     for (var i = 0; i < child.length; i++) {
       console.log("In the loop:", child[i]);
+      if (child[i].className !== undefined) {
 
-      results = results.concat(getElementsByClassName(className, child[i]));
+        results = results.concat(getElementsByClassName(className, child[i]));
 
-      console.log(results);
+        console.log('results of children',results);
+      }
     
      //if it matches class name
        //push the element to the array  
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 
+//input: object
+//output: stringified object
+//constraints: no native methods
+//edge:
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arr;
+
+  //if obj is array
+  if (typeof obj != 'function') {
+    arr = [];
+    if (Array.isArray(obj)) {
+      for (var i = 0; i < obj.length; i++) {
+        arr = arr.concat(stringifyJSON(arr[i]));
+        console.log('array', arr);
+      }
+      
+    } else if (typeof obj === 'object') {
+      for (var key in obj) {
+        arr = arr.concat(stringifyJSON(obj.key));
+        console.log('object', arr);
+      }
+  
+    } else {
+      if (typeof obj === 'string') { 
+        return '"' + obj + '"';
+      } else {
+        var primitive = '';
+        primitive += obj;
+        return primative;
+        console.log('primative', arr);
+      }
+
+    }
+  }
+  return arr;
 };
